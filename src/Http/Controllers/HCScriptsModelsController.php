ea0446af41b34277ea977bdf327c255bd088792c
Jevgenij Dmitrijev <jd@interactivesolutions.lt>
2018-01-22 21:57:54 +0200

    Improved model generation (multilanguage)

--- src/Http/Controllers/HCScriptsModelsController.php
+++ src/Http/Controllers/HCScriptsModelsController.php
@@ -52,12 +52,69 @@ class HCScriptsModelsController
             "namespace" => $this->config->getPackageConfig()->getNamespaceForModel(),
             "model" => $model['modelName'],
             "table" => $model['tableName'],
-            "fields" => '"' . implode('", "', $model['fieldsModel']) . '"'
+            "fields" => '"' . implode('", "', $model['fieldsModel']) . '"',
+            "useNamespaces" => $this->getNameSpaces($model['use']),
+            "useClassNames" => $this->getClassNames($model['use']),
         ];
 
         $modelType = "uuid";
         $destination = $this->config->getDirectory() . "Models/" . $data['model'] . '.php';
 
-        $this->helper->createFileFromTemplate($destination,'service/models/' . $modelType . '.hctpl', $data);
+        $this->helper->createFileFromTemplate($destination, 'service/models/' . $modelType . '.hctpl', $data);
+    }
+
+    /**
+     * @param array $use
+     * @return string
+     */
+    private function getNameSpaces(array $use): string
+    {
+        $value = "";
+
+        foreach ($use as $item) {
+            switch ($item) {
+                case "ownership" :
+
+                    $value .= "use HoneyComb\\Core\\Models\\Traits\\HCOwnership;\r\n";
+                    break;
+
+                case "translations" :
+
+                    $value .= "use HoneyComb\\Core\\Models\\Traits\\HCTranslation;\r\n";
+                    break;
+            }
+        }
+
+        return $value;
+    }
+
+    /**
+     * @param array $use
+     * @return string
+     */
+    private function getClassNames(array $use): string
+    {
+        $value = "";
+
+        foreach ($use as $item) {
+            switch ($item) {
+                case "ownership" :
+
+                    $value .= ", Ownership";
+                    break;
+
+                case "translations" :
+
+                    $value .= ", HCTranslation";
+                    break;
+            }
+        }
+
+        if ($value != "") {
+            $value = "use " . substr($value, 2) . ';';
+        }
+
+
+        return $value;
     }
 }
