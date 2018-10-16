ae84305c87e87acfddcbae8d153e37caf8515580
Mantas Paulauskas <mantopastas@gmail.com>
2018-10-16 15:37:52 +0300

    generation fixes

--- src/Http/Controllers/HCScriptsControllerController.php
+++ src/Http/Controllers/HCScriptsControllerController.php
@@ -64,7 +64,7 @@ class HCScriptsControllerController
             'serviceNs' => $this->config->getPackageConfig()->getNamespaceForService(),
             'serviceName' => $this->config->getServiceName(),
             'serviceNamespace' => $this->config->getPackageConfig()->getNamespaceForService(true),
-            'translationLabel' => $this->config->getTranslation()->getLabelFieldForForm($this->config->getUrl()),
+            'translationLabel' => $this->config->getTranslation()->getLabelFieldForForm('page_title'),
             'urlName' => $this->config->getUrlName(),
             'formName' => $this->config->getRouteName(),
             'actionPrefix' => $this->config->getAclPrefix(),
